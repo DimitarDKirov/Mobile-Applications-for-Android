f3e384fbfbf1c665f9a99775276952fc1a372c15
DimitarDKirov <dimkirov@gmail.com>
2017-02-08 04:06:18 +0200

    Navigation demos updates - added fragments

--- demos/LiveDemoNavigation/app/src/main/java/com/mitko/livedemonavigation/activities/BooksListActivity.java
+++ demos/LiveDemoNavigation/app/src/main/java/com/mitko/livedemonavigation/activities/BooksListActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -12,6 +13,11 @@ import android.widget.Button;
 import android.widget.ListView;
 import android.widget.TextView;
 
+import com.mikepenz.materialdrawer.Drawer;
+import com.mikepenz.materialdrawer.DrawerBuilder;
+import com.mikepenz.materialdrawer.model.PrimaryDrawerItem;
+import com.mikepenz.materialdrawer.model.SecondaryDrawerItem;
+import com.mikepenz.materialdrawer.model.interfaces.IDrawerItem;
 import com.mitko.livedemonavigation.R;
 import com.mitko.livedemonavigation.data.Data;
 import com.mitko.livedemonavigation.models.Book;
@@ -20,17 +26,10 @@ import java.util.List;
 
 public class BooksListActivity extends AppCompatActivity {
     private static final String INTENT_EXTRA_KEY = "args_key";
-    private final Data data;
-
     private int count;
 
     public BooksListActivity() {
-        this(new Data());
-    }
-
-    public BooksListActivity(Data data) {
         super();
-        this.data = data;
         this.count=1;
     }
 
@@ -48,7 +47,7 @@ public class BooksListActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list_book);
-
+    this.setupDrawer();
 //        Intent intent=this.getIntent();
 //        int count=intent.getIntExtra(INTENT_EXTRA_KEY, 1);
 //
@@ -58,38 +57,60 @@ public class BooksListActivity extends AppCompatActivity {
         Button btnGoNext = (Button) this.findViewById(R.id.button_go_next);
         btnGoNext.setOnClickListener(view -> {
             //Intent initialIntent = new Intent(this, BooksListActivity.class);
-            Intent initialIntent = new Intent(this, BookDetailsActivity.class);
+            Intent initialIntent = new Intent(this, BooksListActivity.class);
             initialIntent.putExtra(INTENT_EXTRA_KEY, this.count+1);
             this.startActivity(initialIntent);
         });
+//
+//        ListView lvBooks=(ListView)this.findViewById(R.id.lv_books);
+//        List<Book> books= this.data.getBooks();
+//
+//        ArrayAdapter<Book> booksAdapter=new ArrayAdapter<Book>(this, -1,books){
+//            @NonNull
+//            @Override
+//            public View getView(int position, View convertView, ViewGroup parent) {
+//                View view=convertView;
+//                if(view==null){
+//                    LayoutInflater inflater=LayoutInflater.from(this.getContext());
+//                    view=inflater.inflate(R.layout.item_book, parent, false);
+//                }
+//
+//                TextView tvTitle=(TextView) view.findViewById(R.id.tv_title);
+//                String title=this.getItem(position).getTitle();
+//                tvTitle.setText(title);
+//                return view;
+//            }
+//        };
+//
+//        lvBooks.setAdapter(booksAdapter);
+//
+//        lvBooks.setOnItemClickListener((parent, view, position, id) -> {
+//            Intent intent=new Intent(this, BookDetailsActivity.class);
+//            Book book=books.get(position);
+//            intent.putExtra(BookDetailsActivity.BOOK_KEY, book);
+//            this.startActivity(intent);
+//        });
+    }
 
-        ListView lvBooks=(ListView)this.findViewById(R.id.lv_books);
-        List<Book> books= this.data.getBooks();
+    private void setupDrawer() {
+        //https://github.com/mikepenz/MaterialDrawer
 
-        ArrayAdapter<Book> booksAdapter=new ArrayAdapter<Book>(this, -1,books){
-            @NonNull
-            @Override
-            public View getView(int position, View convertView, ViewGroup parent) {
-                View view=convertView;
-                if(view==null){
-                    LayoutInflater inflater=LayoutInflater.from(this.getContext());
-                    view=inflater.inflate(R.layout.item_book, parent, false);
-                }
+        PrimaryDrawerItem item1 = new PrimaryDrawerItem()
+                .withIdentifier(1)
+                .withName(R.string.drawer_item_home)
+                .withIcon(R.drawable.material_drawer_shadow_bottom);
+        SecondaryDrawerItem item2 = new SecondaryDrawerItem()
+                .withIdentifier(2)
+                .withName(R.string.drawer_item_settings)
+                .withIcon(R.drawable.material_drawer_badge);
 
-                TextView tvTitle=(TextView) view.findViewById(R.id.tv_title);
-                String title=this.getItem(position).getTitle();
-                tvTitle.setText(title);
-                return view;
-            }
-        };
-
-        lvBooks.setAdapter(booksAdapter);
-
-        lvBooks.setOnItemClickListener((parent, view, position, id) -> {
-            Intent intent=new Intent(this, BookDetailsActivity.class);
-            Book book=books.get(position);
-            intent.putExtra(BookDetailsActivity.BOOK_KEY, book);
-            this.startActivity(intent);
-        });
+        Toolbar toolbar=(Toolbar)this.findViewById(R.id.toolbar_drawer);
+        Drawer result = new DrawerBuilder()
+                .withActivity(this)
+                .withToolbar(toolbar)
+                .addDrawerItems(
+                        item1,
+                        item2)
+                .build();
     }
 }
--- demos/LiveDemoNavigation/app/src/main/java/com/mitko/livedemonavigation/fragments/ListBooksFragment.java
+++ demos/LiveDemoNavigation/app/src/main/java/com/mitko/livedemonavigation/fragments/ListBooksFragment.java
@@ -1,19 +1,31 @@
 package com.mitko.livedemonavigation.fragments;
 
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.TextView;
 
 import com.mitko.livedemonavigation.R;
+import com.mitko.livedemonavigation.activities.BookDetailsActivity;
+import com.mitko.livedemonavigation.data.Data;
+import com.mitko.livedemonavigation.models.Book;
+
+import java.util.List;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class ListBooksFragment extends Fragment {
-
+    public static final String  BOOK_KEY= "book_key";
+    private Data data;
 
     public ListBooksFragment() {
         // Required empty public constructor
@@ -23,8 +35,40 @@ public class ListBooksFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        View root = inflater.inflate(R.layout.fragment_list_books, container, false);
+        this.data = new Data();
+
+        ListView lvBooks = (ListView) root.findViewById(R.id.lv_books);
+        List<Book> books = this.data.getBooks();
+
+        ArrayAdapter<Book> booksAdapter = new ArrayAdapter<Book>(root.getContext(), -1, books) {
+            @NonNull
+            @Override
+            public View getView(int position, View convertView, ViewGroup parent) {
+                View view = convertView;
+                if (view == null) {
+                    LayoutInflater inflater = LayoutInflater.from(this.getContext());
+                    view = inflater.inflate(R.layout.item_book, parent, false);
+                }
+
+                TextView tvTitle = (TextView) view.findViewById(R.id.tv_title);
+                String title = this.getItem(position).getTitle();
+                tvTitle.setText(title);
+                return view;
+            }
+        };
+
+        lvBooks.setAdapter(booksAdapter);
+
+        lvBooks.setOnItemClickListener((parent, view, position, id) -> {
+            Intent intent = new Intent(this.getContext(), BookDetailsActivity.class);
+            Book book = books.get(position);
+            intent.putExtra(BOOK_KEY, book);
+            this.startActivity(intent);
+        });
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_list_books, container, false);
+        return root;
     }
 
 }
